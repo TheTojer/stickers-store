5cd64c3b1d25907e6d172a2e9ba9321a1cf0c4e7
Sparky-Fluffy <andrewdev276@gmail.com>
2025-05-28 15:27:36 +0700

    Init windows socket api struct WSADATA.

--- code-example/main.c
+++ code-example/main.c
@@ -4,5 +4,12 @@
 
 int main(void)
 {
+    WSADATA wsaData;
+    int res = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (res != 0)
+    {
+        fprintf(stderr, "Failed init sockets.\n");
+        return 1;
+    }
     return 0;
 }
